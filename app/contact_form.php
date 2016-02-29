16a51a7d2dbd75020e325751a64a18557d1636fc
thephotobee <info.thephotobee@gmail.com>
2016-02-29 11:47:48 -0800

    stuff

--- app/contact_form.php
+++ app/contact_form.php
@@ -1,18 +1,61 @@
-<?php
+<html>
 
-if(isset($_POST["submit"])) {
-$to = "stephievictoria@gmail.com";
-$contactpagesubject = $_POST["contactpagesubject"];
-$contactfirstname = $_POST["contactpagefirstname"];
-$contactpagelastname = $_POST["contactpagelastname"];
-$contactpageemail = $_POST["contactpageemail"];
-$contactpagemessage = 'First Name: '.$_POST["contactpagefirstname"]."\n"
-.'Last Name: '.$_POST["contactpagelastname"]."\n"
-.'Email: '.$_POST["contactpageemail"]."\n"
-.'Message: '.$_POST["contactpagemessage"];
-$headers = 'From: Weekend-Pants-Contact';
+<head>
+    <title>Thanks For Contacting Us</title>
+</head>
 
+<body>
+    <?php
+  // Change this to YOUR address
+  $recipient = 'stephievictoria@gmail.com';
+  $email = $_POST['email'];
+  $realName = $_POST['realname'];
+  $subject = $_POST['subject'];
+  $body = $_POST['body'];
+  # We'll make a list of error messages in an array
+  $messages = array();
+# Allow only reasonable email addresses
+if (!preg_match("/^[\w\+\-.~]+\@[\-\w\.\!]+$/", $email)) {
+$messages[] = "That is not a valid email address.";
 }
-mail ($to, $contactpagesubject, $contactpagemessage, $headers);
-header('location: redirectcontactform.php');
+# Allow only reasonable real names
+if (!preg_match("/^[\w\ \+\-\'\"]+$/", $realName)) {
+$messages[] = "The real name field must contain only " .
+"alphabetical characters, numbers, spaces, and " .
+"reasonable punctuation. We apologize for any inconvenience.";
+}
+# CAREFUL: don't allow hackers to sneak line breaks and additional
+# headers into the message and trick us into spamming for them!
+$subject = preg_replace('/\s+/', ' ', $subject);
+# Make sure the subject isn't blank afterwards!
+if (preg_match('/^\s*$/', $subject)) {
+$messages[] = "Please specify a subject for your message.";
+}
+
+$body = $_POST['body'];
+# Make sure the message has a body
+if (preg_match('/^\s*$/', $body)) {
+$messages[] = "Your message was blank. Did you mean to say " .
+"something?"; 
+}
+  if (count($messages)) {
+    # There were problems, so tell the user and
+    # don't send the message yet
+    foreach ($messages as $message) {
+      echo("<p>$message</p>\n");
+    }
+    echo("<p>Click the back button and correct the problems. " .
+      "Then click Send Your Message again.</p>");
+  } else {
+    # Send the email - we're done
+mail($recipient,
+      $subject,
+      $body,
+      "From: $realName <$email>\r\n" .
+      "Reply-To: $realName <$email>\r\n"); 
+    echo("<p>Your message has been sent. Thank you!</p>\n");
+  }
 ?>
+</body>
+
+</html>
